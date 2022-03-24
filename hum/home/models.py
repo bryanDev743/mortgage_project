be055f87d961e386c714fa8ed7a40fc785e83a2c
bryanDev743 <bryandev743@gmail.com>
2022-03-24 17:01:51 -0500

    Models added

--- hum/home/models.py
+++ hum/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    uname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250)
+    passw = models.CharField(max_length=50)
+    states = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
+    phone_n = models.CharField(max_length=15)
+    dob = models.CharField(max_length=15)
+
+    #return names in admin
+    # def__str__(self):
+    #     return self.uname + ' ' + self.email
+
+
--- hum/home/views.py
+++ hum/home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import User
 
 def home_view(request):
+
+    #alldata = User.objects.all
+
     return render(request,'index.html',{})
+
+
 
 
 #def 
