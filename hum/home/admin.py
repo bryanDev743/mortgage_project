d169a5034a6e94cd8356ff4345f7ee573cb270e0
Cantogen <47679974+Cantogen@users.noreply.github.com>
2022-04-25 18:00:38 -0500

    - Patch Cleanup #3
    - Did all corrections from previous patch
    - Including fresh project folder and all html files in uniform location
    - Also unified all views and urls to same spot for easier access and connection
    - That's it, I just had to get the hang of updated the branch

--- hum/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Finance, Bank, Loan, User_has_Loan, Bank_offers_Loan
-
-myModels = [User, Finance, Bank, Loan, User_has_Loan, Bank_offers_Loan]
-
-admin.site.register(myModels)
-#admin.site.register(Loan, User_has_Loan, Bank_offers_Loan)
-
-# Register your models here.
---
+++ project/urls.py
@@ -0,0 +1,32 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings #BS: for static files
+from django.conf.urls.static import static #BS: for static files
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import include
+
+from . import views
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', views.home_view, name ='home'),
+    path('signup', views.signup_view, name = 'signup'),
+    path('login', views.login_view, name = 'login'),
+    path('user', views.user_view, name = 'user'),
+    path('customer_form', views.create_customer, name='create_customer'),
+]# + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) #BS: for static files
