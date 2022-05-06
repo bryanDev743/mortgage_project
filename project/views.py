0c37b1a489b70449d6fb622e72430e80ab1b66c5
Cantogen <47679974+Cantogen@users.noreply.github.com>
2022-05-06 14:29:52 -0500

    Added Link to Add Client page
    - Added link
    - Fixed and added logout
    - Changed spacing in navbar slightly

--- project/views.py
+++ project/views.py
@@ -27,20 +27,21 @@ def home_view(request):
 
 # Using old signup as new database entry
 
-def new_client(request):
+def new_client_view(request):
     submitted = False
     form = ClientForm(request.POST)
     if request.method == "POST":
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/signup_success')
+            messages.success(request, 'Successful Client Registry!')
+            return HttpResponseRedirect('/new_client')
         else:
             form = ClientForm
             if 'submitted' in request.GET:
                 submitted = True
             messages.info(request, 'That Username or Email is taken!')
 
-    return render(request,'signup.html',{'form': form, 'submitted': submitted})
+    return render(request,'new_client.html',{'form': form, 'submitted': submitted})
 
 def signup_success_view(request):
     return render(request, 'signup_success.html', {}) #returns the index.html template
@@ -48,6 +49,11 @@ def signup_success_view(request):
 def login_view(request):
         form = ClientForm()
         return render(request,'login.html',{'form': form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout Successful!')
+    return render(request, 'home.html', {})
 
 def user_view(request):
     return render(request, 'user.html', {}) #returns the index.html template
